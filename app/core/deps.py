b9652d1eba321ca0d718212551669043800f440f
ikuko-otani <ikuko.otani.dev@gmail.com>
2026-05-22 15:05:53 +0900

    fix(s3-5): return user in get_current_user function after validation

--- app/core/deps.py
+++ app/core/deps.py
@@ -43,6 +43,7 @@ async def get_current_user(
         raise credentials_exception
     if not user.is_active:
         raise credentials_exception  # not 403
+    return user
 
 
 CurrentUser = Annotated[User, Depends(get_current_user)]
